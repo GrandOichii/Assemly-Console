938a013b5985969542ed97003f261b5da25e3a23
oichii <i.hawk@yandex.ru>
2020-09-18 15:56:29 +0300

    Added console1.asm, writes 'Hello, world' to the console Added console2.asm, asks the user for his name and outputs the users name, format: 'Your name is %s'

---
+++ console2/console2.asm
@@ -0,0 +1,43 @@
+format PE console
+entry start
+include 'win32a.inc'
+
+section '.data' data readable writeable
+
+        prompt db 'What is your name? ', 0
+        outstr db 'You name is %d', 0
+        inputStr db '%d', 0
+
+        name db ?
+
+        NULL = 0
+
+
+section '.code' code readable executable
+        start:
+                push prompt
+                call [printf]
+
+                push name
+                push inputStr
+                call [scanf]
+
+                push name
+                push outstr
+                call [printf]
+
+                call [getch]
+
+                push NULL
+                call [ExitProcess]
+
+section 'idata' import data readable
+        library kernel, 'kernel32.dll', \
+                msvcrt, 'msvcrt.dll'
+        import kernel, \
+               ExitProcess, 'ExitProcess'
+
+        import msvcrt, \
+               scanf, 'scanf', \
+               printf, 'printf', \
+               getch, '_getch'
